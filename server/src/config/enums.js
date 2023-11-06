ebd7c6b02b0431a41839ab09bfb495b1da257170
EbnDaif <51683663+EbnDaif@users.noreply.github.com>
2023-11-06 07:46:59 +0200

    Revert "courses - router,enums,model,joi"

--- server/src/config/enums.js
+++
@@ -1,2 +0,0 @@
-exports.enum_coursesLevels = ['beginner', 'intermediate', 'advanced'];
-exports.enum_coursesStatus = ['Upcoming', 'Ongoing', 'ended'];
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,6 +1,5 @@
 const router=require("express").Router()
 router.use("/auth", require("./authRoutes"))
 router.use("/users", require("./userRoutes"))
-router.use('/courses', require('./course.route'));
 
 module.exports=router
