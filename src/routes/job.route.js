112cfe28c613eb4f9053681ae8dbf59d0317b543
TahaHamdy02 <tahahamdy34@gmail.com>
2023-11-14 06:58:48 +0200

    merge jobs

--- src/routes/job.route.js
+++ src/routes/job.route.js
@@ -6,12 +6,12 @@ const {createJobValidation} = require('../validation/job.validation')
 const {updateJobValidation} = require('../validation/job.validation')
 const { validationMiddleware } = require('../middlewares/validationMiddleware')
 const validateObjectId = require('../middlewares/validateObjectIdMiddleware');
-const {authenticate} =require('../middlewares/authenticateMiddleware')
+const {authenticate, authorizeAdmin} =require('../middlewares/authenticateMiddleware')
 
-router.post('/create-job',authenticate,upload.single('companyLogo') ,validationMiddleware(createJobValidation)  , createJob)
+router.post('/create-job',authorizeAdmin,upload.single('companyLogo') ,validationMiddleware(createJobValidation)  , createJob)
 router.get('/get-job/:id',validateObjectId,authenticate , getJob)
 router.get('/get-all-jobs',authenticate , getAllJobs)
-router.patch('/update-job/:id' , validateObjectId,authenticate, validationMiddleware(updateJobValidation) , upload.single('companyLogo') , updateJob)
-router.delete('/delete-job/:id' , validateObjectId ,authenticate, deleteJob)
+router.patch('/update-job/:id' , validateObjectId,authorizeAdmin, validationMiddleware(updateJobValidation) , upload.single('companyLogo') , updateJob)
+router.delete('/delete-job/:id' , validateObjectId ,authorizeAdmin, deleteJob)
 
 module.exports = router;
