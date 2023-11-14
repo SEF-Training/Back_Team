85db6996803fd6e594efe3954355678fa097874a
e-mustafa <eng.mustafax@gmail.com>
2023-11-14 16:01:48 +0200

    enums

--- server/src/config/enums.js
+++ server/src/config/enums.js
@@ -1,2 +1,13 @@
 exports.enum_coursesLevels = ['beginner', 'intermediate', 'advanced'];
 exports.enum_coursesStatus = ['upcoming', 'ongoing', 'ended'];
+
+exports.enum_ArticlesStatus = ['published', 'draft'];
+exports.enum_ArticlesCategory = [
+	'tech',
+	'business',
+	'security',
+	'sports',
+	'medical',
+	'startups',
+	'about',
+];
