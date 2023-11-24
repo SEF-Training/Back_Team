e22753208d033e159ddcce4a7e0d68a6d9512482
TahaHamdy-MernDev <TahaHamdy.mern@gmail.com>
2023-11-25 01:38:42 +0200

    login current-user/ fix cookies isuues

--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const router=require("express").Router()
-router.use("/auth", require("./authRoutes"))
-router.use("/users", require("./userRoutes"))
-router.use('/courses', require('./course.route'));
-router.use('/jobs' , require('./job.route.js'))
-router.use('/certificates', require('./certificate.route'));
-router.use('/articles', require('./article.route'));
-module.exports=router
