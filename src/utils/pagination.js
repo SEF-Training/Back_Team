25583ca3006c2cf34441bf0c4859b9a76ca63ae6
TahaHamdy-MernDev <TahaHamdy.mern@gmail.com>
2023-11-30 09:48:07 +0200

    updates

--- src/utils/pagination.js
+++ src/utils/pagination.js
@@ -3,13 +3,13 @@ async function paginate(model, req) {
     const limit = +req.query.limit || 10;
     const startIndex = (page - 1) * limit;
     const { fieldName, fieldValue, searchBy, searchValue } = req.query;
-    // console.log({ fieldName, fieldValue, searchBy, searchValue })
+    console.log({ fieldName, fieldValue, searchBy, searchValue })
     const dynamicQuery = fieldValue && fieldName ? { [fieldName]: fieldValue } : {};
     const searchQuery = searchBy ? { [searchBy]: { $regex: new RegExp(searchValue, 'i') } } : {};
     const transformedQuery = { ...dynamicQuery, ...searchQuery };
     const totalDocs = await model.countDocuments(transformedQuery);
     const pagination = { total: totalDocs, limit, page, pages: Math.ceil(totalDocs / limit) };
-  
+  console.log({transformedQuery,totalDocs})
     if (startIndex > totalDocs) { return { error: 'Page not found' }; }
     const data = await model.find({ ...transformedQuery, _id: { $ne: req.user._id } })
       .limit(limit)
